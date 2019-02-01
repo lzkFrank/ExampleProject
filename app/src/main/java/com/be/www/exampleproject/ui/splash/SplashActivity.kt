acb70aa7395e77e0537b6794a40cff09589f871d
frankliu <lzkFrank@163.com>
2019-02-01 16:36:31 +0800

    android9.0全屏适配

--- app/src/main/java/com/be/www/exampleproject/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/be/www/exampleproject/ui/splash/SplashActivity.kt
@@ -6,24 +6,45 @@ import android.os.Bundle
 import android.os.Handler
 import com.be.www.exampleproject.R
 import com.be.www.exampleproject.ui.main.MainActivity
+import android.view.WindowManager
+import android.os.Build
+
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //防止个别机型重复初始化启动页面
+        fullP()
+        preventReset()
+        setContentView(R.layout.activity_splash)
+    }
+
+    /**
+     * 防止个别机型重复初始化启动页面
+     * */
+    private fun preventReset() {
         if (!this.isTaskRoot && intent != null) {
             val action = intent.action
             if (intent.hasCategory(Intent.CATEGORY_LAUNCHER) && Intent.ACTION_MAIN == action) {
                 finish()
             }
         }
-        setContentView(R.layout.activity_splash)
     }
 
     override fun onStart() {
         super.onStart()
         Handler().postDelayed({ startActivity(Intent(this@SplashActivity, MainActivity::class.java)) }
                 , 1500L)
+    }
+
+    /**
+     * 9.0全屏视屏
+     * */
+    private fun fullP() {
+        if (Build.VERSION.SDK_INT >= 28) {
+            val lp = window.attributes
+            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            window.attributes = lp
+        }
     }
 }
